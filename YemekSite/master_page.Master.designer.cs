0c734f68b687370f408ac23d01b96954321fe1e8
cemlevent54 <cemlevent54@gmail.com>
2024-07-12 12:44:56 +0300

    login kısmına menu eklendi 12.07.2024

--- YemekSite/master_page.Master.designer.cs
+++ YemekSite/master_page.Master.designer.cs
@@ -141,13 +141,13 @@ namespace YemekSite
         protected global::System.Web.UI.WebControls.Label lbl_user_name;
 
         /// <summary>
-        /// lnkbtn_logout control.
+        /// LinkButton1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.LinkButton lnkbtn_logout;
+        protected global::System.Web.UI.WebControls.LinkButton LinkButton1;
 
         /// <summary>
         /// ContentPlaceHolder1 control.
